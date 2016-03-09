a1314d7e66165ad7fb03581be846983a3b4f5146
Scott Schneider <sschneid@gmail.com>
2016-03-09 13:41:50 -0800

    Tests for shakedown.dcos.service.get_service_tasks() and get_service_ips()

--- tests/acceptance/test_dcos_service.py
+++ tests/acceptance/test_dcos_service.py
@@ -3,8 +3,30 @@ from shakedown.dcos import service as svc
 
 
 def test_get_service_framework_id():
-    pkg.install_package_and_wait('jenkins')
-    assert svc.get_service_framework_id('jenkins') != False
-    
-    pkg.uninstall_package_and_wait('jenkins')
-    assert svc.get_service_framework_id('jenkins') == False
+    assert svc.get_service_framework_id('spark') == None
+
+    pkg.install_package_and_wait('spark')
+    framework_id = svc.get_service_framework_id('spark')
+    print('framework_id: ' + framework_id)
+    assert framework_id != None
+
+    pkg.uninstall_package_and_wait('spark')
+    assert svc.get_service_framework_id('spark') == None
+
+
+def test_get_service_tasks():
+    pkg.install_package_and_wait('hdfs')
+    service_tasks = svc.get_service_tasks('marathon')
+    assert service_tasks != None
+
+    pkg.uninstall_package_and_wait('hdfs')
+
+
+def test_get_service_ips():
+    pkg.install_package_and_wait('chronos')
+
+    # Get all IPs associated with the 'chronos' task running in the 'marathon' service
+    service_ips = svc.get_service_ips('marathon', 'chronos')
+    print('service_ips: ' + str(service_ips))
+
+    pkg.uninstall_package_and_wait('chronos')

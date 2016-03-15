a6d032ea84e64252a5babb8d91bd5a07c5db1b6f
Scott Schneider <sschneid@gmail.com>
2016-03-15 11:39:25 -0700

    Import syntax fixups
    - `import shakedown` in all shakedown libraries
    - `from shakedown import *` in all tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,16 @@
 from click.testing import CliRunner
 
-import shakedown
-
-from shakedown.cli.main import cli
+from shakedown import *
 
 
 def test_cli_require_dcos_uri():
     runner = CliRunner()
-    result = runner.invoke(cli)
+    result = runner.invoke(cli.main.cli)
     assert result.exit_code == 1
 
 def test_cli_version():
     runner = CliRunner()
-    result = runner.invoke(cli, ['--version'])
+    result = runner.invoke(cli.main.cli, ['--version'])
     assert result.exit_code == 0
     assert result.output == "shakedown, version " + shakedown.VERSION + "\n"
 

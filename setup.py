15a42b44eafd701933092e0630f18a2bf1891ebd
Scott Schneider <sschneid@gmail.com>
2016-06-09 16:11:42 -0700

    Update setup files for PyPi release

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 
 setup(name='shakedown',
-      version='0.0.1',
-      description=u"shakedown",
-      long_description=u"ssshhhaaakkkeeedddooowwwnnn",
+      version='1.0.0',
+      description=u"DC/OS testing harness and library",
+      long_description=u"A tool and library to abstract common DC/OS-related tasks.",
       classifiers=[],
       keywords='',
       author=u"Mesosphere QE",
       author_email='qe-team@mesosphere.com',
-      url='https://...',
+      url='https://github.com/dcos/shakedown',
       license='Apache 2',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,

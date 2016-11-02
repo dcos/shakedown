c63effa0c1ea4c4adff61608b81df1056028071f
Scott Schneider <scott@mesosphere.com>
2016-11-02 10:27:35 -0700

    Bump dcoscli version to `0.4.14`

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='dcos-shakedown',
       zip_safe=False,
       install_requires=[
           'click',
-          'dcoscli==0.4.13',
+          'dcoscli==0.4.14',
           'paramiko',
           'pytest',
           'scp'

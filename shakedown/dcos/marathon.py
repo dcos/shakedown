98c9fca0b63956a12db9ab070e42942e86a07363
Ken Sipe <kensipe@gmail.com>
2017-04-20 16:10:26 -0500

    adding in mom context and mom version details

--- shakedown/dcos/marathon.py
+++ shakedown/dcos/marathon.py
@@ -1,7 +1,9 @@
 from distutils.version import LooseVersion
-from dcos import marathon
+from dcos import marathon, config
 from shakedown.dcos.spinner import *
+from shakedown.dcos.service import service_available_predicate
 from shakedown import *
+from six.moves import urllib
 
 import pytest
 
@@ -26,6 +28,41 @@ def marathon_version():
 
 def marthon_version_less_than(version):
     return marathon_version() < LooseVersion(version)
+
+
+def mom_version(name='marathon-user'):
+    """Returns the version of marathon on marathon.
+    """
+    if service_available_predicate(name):
+        with marathon_on_marathon(name):
+                return marathon_version()
+    else:
+        # We can either skip the corresponding test by returning False
+        # or raise an exception.
+        print('WARN: {} MoM not found. Version is None'.format(name))
+        return None
+
+
+
+def mom_version_less_than(version, name='marathon-user'):
+    """ Returns True if MoM with the given {name} exists and has a version less
+        than {version}. Note that if MoM does not exist False is returned.
+
+        :param version: required version
+        :type: string
+        :param name: MoM name, default is 'marathon-user'
+        :type: string
+        :return: True if version < MoM version
+        :rtype: bool
+    """
+    if service_available_predicate(name):
+        return mom_version() < LooseVersion(version)
+    else:
+        # We can either skip the corresponding test by returning False
+        # or raise an exception.
+        print('WARN: {} MoM not found. mom_version_less_than({}) is False'.format(name, version))
+        return False
+
 
 def deployment_predicate(app_id=None):
     return len(marathon.create_client().get_deployments(app_id)) == 0
@@ -60,3 +97,23 @@ def is_app_healthy(app_id):
         return app["tasksHealthy"] == app["instances"]
     else:
         return app["tasksRunning"] == app["instances"]
+
+
+@contextlib.contextmanager
+def marathon_on_marathon(name='marathon-user'):
+    """ Context manager for altering the marathon client for MoM
+    :param name: service name of MoM to use
+    :type name: str
+    """
+
+    toml_config_o = config.get_config()
+    dcos_url = config.get_config_val('core.dcos_url', toml_config_o)
+    service_name = 'service/{}/'.format(name)
+    marathon_url = urllib.parse.urljoin(dcos_url, service_name)
+    config.set_val('marathon.url', marathon_url)
+
+    try:
+        yield
+    finally:
+        # return config to previous state
+        config.save(toml_config_o)

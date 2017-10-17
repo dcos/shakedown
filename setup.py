c92d0e11522f1ed09e97bfe4fa80072b0a0f7296
Ken Sipe <kensipe@gmail.com>
2017-10-17 13:21:02 -0500

    adding classifiers to the setup to specify the programming langauge to be used with pipenv

--- setup.py
+++ setup.py
@@ -5,7 +5,25 @@ setup(name='dcos-shakedown',
       version='1.4.8',
       description=u"DC/OS testing harness and library",
       long_description=u"A tool and library to abstract common DC/OS-related tasks.",
-      classifiers=[],
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: User Interfaces',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+      ],
       keywords='',
       author=u"Mesosphere QE",
       author_email='qe-team@mesosphere.com',

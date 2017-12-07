1c160f016ce7b1d3802ba1963c7ce536f21da822
Ken Sipe <kensipe@gmail.com>
2017-12-07 16:35:03 -0600

    adding a pause char for skip

--- shakedown/cli/main.py
+++ shakedown/cli/main.py
@@ -181,6 +181,8 @@ def cli(**args):
                 schr = fchr('FF')
             elif state == 'pass':
                 schr = fchr('PP')
+            elif state == 'skip':
+                schr = fchr('SK')
             else:
                 schr = ''
 

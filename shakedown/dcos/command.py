297987e92a5989b99400d674753065d6c5537ad4
Colin C <colin@mesosphere.com>
2018-04-10 17:10:21 -0700

    New connection cache
    It was possible with the old implementation for a None connection to
    be returned and cached forever.

--- shakedown/dcos/command.py
+++ shakedown/dcos/command.py
@@ -1,18 +1,74 @@
 import shlex
 import subprocess
-from functools import lru_cache
-
 import time
-
+from _thread import RLock
+from functools import wraps
 from select import select
+
+import paramiko
 from dcos.errors import DCOSException
-from .helpers import validate_key, try_close, get_transport, start_transport
 
 import shakedown
+from .helpers import validate_key, try_close, get_transport, start_transport
 
 
-@lru_cache(maxsize=None)
-def _get_connection(host, username, key_path):
+def connection_cache(func: callable):
+    """Connection cache for SSH sessions. This is to prevent opening a
+     new, expensive connection on every command run."""
+    cache = dict()
+    lock = RLock()
+
+    @wraps(func)
+    def func_wrapper(host: str, username: str, *args, **kwargs):
+        key = "{h}-{u}".format(h=host, u=username)
+        if key in cache:
+            # connection exists, check if it is still valid before
+            # returning it.
+            conn = cache[key]
+            if conn and conn.is_active() and conn.is_authenticated():
+                return conn
+            else:
+                # try to close a bad connection and remove it from
+                # the cache.
+                if conn:
+                    try_close(conn)
+                del cache[key]
+
+        # key is not in the cache, so try to recreate it
+        # it may have been removed just above.
+        if key not in cache:
+            conn = func(host, username, *args, **kwargs)
+            if conn is not None:
+                cache[key] = conn
+            return conn
+
+        # not sure how to reach this point, but just in case.
+        return None
+
+    def get_cache() -> dict:
+        return cache
+
+    def purge(key: str=None):
+        with lock:
+            if key is None:
+                conns = [(k, v) for k, v in cache.items()]
+            elif key in cache:
+                conns = ((key, cache[key]), )
+            else:
+                conns = list()
+
+            for k, v in conns:
+                try_close(v)
+                del cache[k]
+
+    func_wrapper.get_cache = get_cache
+    func_wrapper.purge = purge
+    return func_wrapper
+
+
+@connection_cache
+def _get_connection(host, username: str, key_path: str) \
+        -> paramiko.Transport or None:
     """Return an authenticated SSH connection.
 
     :param host: host or IP of the machine
@@ -22,6 +78,7 @@ def _get_connection(host, username, key_path):
     :param key_path: path to the SSH private key for SSH auth
     :type key_path: str
     :return: SSH connection
+    :rtype: paramiko.Transport or None
     """
     if not username:
         username = shakedown.cli.ssh_user
@@ -29,7 +86,7 @@ def _get_connection(host, username, key_path):
         key_path = shakedown.cli.ssh_key_file
     key = validate_key(key_path)
     transport = get_transport(host, username, key)
-    
+
     if transport:
         transport = start_transport(transport, username, key)
         if transport.is_authenticated():
@@ -38,7 +95,7 @@ def _get_connection(host, username, key_path):
             print("error: unable to authenticate {}@{} with key {}".format(username, host, key_path))
     else:
         print("error: unable to connect to {}".format(host))
-    
+
     return None
 
 

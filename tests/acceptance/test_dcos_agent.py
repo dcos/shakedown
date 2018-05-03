50cd4715c80eae5b58f010ac2c111932d05b7048
Ken Sipe <ken@mesosphere.io>
2018-05-03 14:23:41 -0500

    removing odd exception handling.  assert failure will provide more information

--- tests/acceptance/test_dcos_agent.py
+++ tests/acceptance/test_dcos_agent.py
@@ -18,11 +18,7 @@ def test_get_public_agents_public_ip():
     public_ips = get_public_agents_public_ip()
 
     assert isinstance(public_ips, list)
-
-    try:
-        assert socket.inet_aton(public_ips[0])
-    except:
-        assert False
+    assert socket.inet_aton(public_ips[0])
 
 
 def test_get_private_agents():
